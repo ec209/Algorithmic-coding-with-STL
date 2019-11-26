cb643cc006373155238bda1228c01eb75c8f3d7f
TAEGI <felimox.kim@gmail.com>
2019-11-25 20:40:47 -0500

    string

---
+++ baek10172.cpp
@@ -0,0 +1,21 @@
+//
+//  baek10172.cpp
+//  
+//
+//  Created by TAEGIKIM on 27/07/2017.
+//
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    printf("%s\n","|\\_/|");
+    printf("%s\n","|q p|   /}");
+    printf("%s\n","( 0 )\"\"\"\\");
+    printf("%s\n","|\"^\"\`    |");
+    printf("%s\n","||_/=\\\\__|");
+}
